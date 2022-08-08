fc7009170f76c16672a4a417cf1b8a8f0aee3a26
Michal Vavřík <mvavrik@redhat.com>
2022-08-08 23:52:40 +0200

    Reproducer for issue 26553

---
+++ src/main/java/org/acme/ReactiveGreetingResource.java
@@ -0,0 +1,19 @@
+package org.acme;
+
+import io.smallrye.common.annotation.NonBlocking;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/hello")
+public class ReactiveGreetingResource {
+
+    @NonBlocking
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hello() {
+        return "Hello RESTEasy Reactive";
+    }
+}
